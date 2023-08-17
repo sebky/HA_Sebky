d2ab5fa9d91d1d2cad2e7c5b362608c1dd75c500
brotha.midnight <115979416+sebky@users.noreply.github.com>
2023-08-17 09:10:32 +0200

    Update test_run.py

--- HA-17.09.22/test_run.py
+++ HA-17.09.22/test_run.py
@@ -10,7 +10,7 @@ class TestSub(unittest.TestCase):
         self.assertEqual(sub(10, 7), 5)
 
     def test_add_function_with_floats(self):
-        self.assertAlmostEqual(sub(8.9, 10.5), 1.6)
+        self.assertAlmostEqual(sub(15.5, 10.5), 5.0)
 
 
 if __name__ == "__main__":
